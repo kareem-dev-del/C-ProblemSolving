6f31b8bd8567f0f8623d09a4ffe47f562145d169
kareem-dev-del <krym33632@gmail.com>
2025-12-07 12:23:51 +0200

    first commit

--- memoryAllocation.c
+++ memoryAllocation.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    int size=2;
-    int count=0;
+int main()
+{
+    int size = 2;
+    int count = 0;
 
-    int *arr = (int*) malloc(size * sizeof(int));
-    if(arr == NULL) {
+    int *arr = (int *)malloc(size * sizeof(int));
+    if (arr == NULL)
+    {
         printf("Error: Memory not allocated\n");
         return 1;
     }
-
 
     int num;
     printf("Enter numbers (enter -1 to stop):\n");
@@ -19,18 +20,30 @@ int main(){
     {
         scanf("%d", &num);
         if (num == -1)
-        break;
+            break;
 
-        arr[count]=num;
-        count++; 
+        if (count == size)
+        {
+            size = size * 2;
+
+            arr = (int *)realloc(arr, size * sizeof(int));
+            if (arr == NULL)
+            {
+                printf("Reallocation failed\n");
+                return 1;
+            }
+        }
+
+        arr[count] = num;
+        count++;
     }
 
-
-    if(count == size) {
-    size = size * 2; 
-
-    arr = (int*) realloc(arr, size * sizeof(int);)
-    
+    printf("\nYou entered:\n");
+    for (int i = 0; i < count; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    free(arr);
 
     return 0;
 }
